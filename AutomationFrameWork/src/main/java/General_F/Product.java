5e0c3243e5f66b8cbf7fa496929a7654fcda461f
hp <hp@DESKTOP-AF00V8D>
2025-02-28 10:20:21 -0500

    COMMIT

--- AutomationFrameWork/src/main/java/General_F/Product.java
+++ AutomationFrameWork/src/main/java/General_F/Product.java
@@ -6,8 +6,8 @@ import org.openqa.selenium.support.FindBy;
 public class Product {
 	
 	@FindBy(id = "user_name")
+	private WebElement createproductImgBtn;
 	
-	private WebElement createproductImgBtn; 
 	
 	@FindBy (name = " search ") 
 	private WebElement  ele2;
