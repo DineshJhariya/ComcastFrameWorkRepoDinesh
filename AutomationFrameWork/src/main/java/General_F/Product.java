896339526d3cfb59db46fda75953a9a4ae1fa766
hp <hp@DESKTOP-AF00V8D>
2025-02-27 19:40:24 -0500

    Added in pom class

---
+++ AutomationFrameWork/src/main/java/General_F/Product.java
@@ -0,0 +1,12 @@
+package General_F;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Product {
+	
+	@FindBy(id = "user_name")
+	
+	private WebElement createproductImgBtn;
+
+}
